0356588db5eceaf4e54d25bbb294765bdaab3262
slvmgsf <34591196+slvmgsf@users.noreply.github.com>
2018-04-06 22:11:17 +0800

    Mengisi Nilai Atribut

--- src/praktek01/KomputerAksi3.java
+++ src/praktek01/KomputerAksi3.java
@@ -12,5 +12,11 @@ package praktek01;
 public class KomputerAksi3 {
      public static void main(String[] args) {
          Komputer Ane = new Komputer();
+         
+        Ane.Monitor="HP";
+        Ane.Mouse="Mouse Kabel";
+        Ane.RAM="8GB";
+        Ane.Warna="Putih";
+        Ane.OS="Windows 10";
      }
 }
