80d6847b02beed445990d87f6508cdbe7f6a67bd
slvmgsf <34591196+slvmgsf@users.noreply.github.com>
2018-04-06 21:54:12 +0800

    Membuat Class Aksi dan Method Main 2

---
+++ src/praktek01/KomputerAksi2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author C55D
+ */
+public class KomputerAksi2 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
