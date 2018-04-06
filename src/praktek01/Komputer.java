1cdb0c662e53aba5815d8ad354a4b72e22e83a50
slvmgsf <34591196+slvmgsf@users.noreply.github.com>
2018-04-06 21:31:14 +0800

    Membuat Attribute Dan Behaviour

--- src/praktek01/Komputer.java
+++ src/praktek01/Komputer.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author C55D
  */
 public class Komputer {
-    
+  String Monitor;
+  String Mouse;
+  String RAM;
+  String Warna;
+  String OS;
+
+  void cetakInfo () {
+      System.out.println("Monitor :"+Monitor);
+      System.out.println("Mouse :"+Mouse);
+      System.out.println("RAM :"+RAM);
+      System.out.println("Warna :"+Warna);
+      System.out.println("OS :"+OS);
+  }  
 }
